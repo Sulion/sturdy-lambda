41d77e4b4a56553482efe11c6553c840121ca2df
Michael Rykov <michael.rykov@gmail.com>
2016-09-03 15:47:30 +0300

    Initialize repository

---
+++ src/main/java/ru/rykov/rdd/App.java
@@ -0,0 +1,20 @@
+package ru.rykov.rdd;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String appName = "sturdy-lambda";
+        String master = "local";
+        SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+    }
+}
